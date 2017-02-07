08c62747e89d7e896cafb1eaf6b3a864a3c99abc
flyhooo <807694432@qq.com>
2017-02-07 15:19:07 +0800

    登陆

---
+++ app/src/main/java/com/example/administrator/testdemo/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.testdemo;
+
+import com.example.mylibrary.base.CM_BaseActivity;
+
+/**
+ * Created by Flyhooo on 2017/2/7 15:15
+ */
+
+public class LoginActivity extends CM_BaseActivity {
+    @Override
+    public int getContentLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
